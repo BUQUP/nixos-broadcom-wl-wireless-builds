088454d2da906b00cdc6b1a6dcd7de1c335eabc8
jmshtr <157889640+jmshtr@users.noreply.github.com>
2024-02-23 07:36:09 +0000

    Update broadcom-wl-wireless-build-iso.nix

--- broadcom-wl-wireless-build-iso.nix
+++ broadcom-wl-wireless-build-iso.nix
@@ -2,11 +2,23 @@
 
 {
   imports = [
+    # Minimal installation from CD/DVD module
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
   ];
 
+  # Allow unfree packages in Nixpkgs
   nixpkgs.config.allowUnfree = true;
-  boot.kernelModules = [ "wl" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
-  boot.blacklistedKernelModules = [ "b43" "bcma" ];
+
+  # Boot configuration
+  boot = {
+    kernelModules = [ "wl" ];
+    extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+    blacklistedKernelModules = [ "b43" "bcma" ];
+  };
+
+  # Build ISO image
+  isoImage = pkgs.writeScript "build-iso.sh" ''
+    #!/bin/sh
+    sudo nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
+  '';
 }
