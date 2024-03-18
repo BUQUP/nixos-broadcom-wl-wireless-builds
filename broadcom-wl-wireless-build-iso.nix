5b3276158b10b86cc613f9228518fa734947e01e
James Hunter <157889640+jmshtr@users.noreply.github.com>
2024-03-18 09:12:14 +0000

    Update broadcom-wl-wireless-build-iso.nix

--- broadcom-wl-wireless-build-iso.nix
+++ broadcom-wl-wireless-build-iso.nix
@@ -4,14 +4,21 @@
   imports = [
     # Minimal installation from CD/DVD module
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+
+    # GNOME graphical installation from CD/DVD module
+    #<nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
   ];
 
   # Allow unfree packages in Nixpkgs
   nixpkgs.config.allowUnfree = true;
 
+  # Allow broken packages (commented out by default)
+  #nixpkgs.config.allowBroken = true;
+
   # Boot configuration
   boot = {
     kernelModules = [ "wl" ];
+    kernelPackages = pkgs.linuxPackages_latest;
     extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
     blacklistedKernelModules = [ "b43" "bcma" ];
   };
